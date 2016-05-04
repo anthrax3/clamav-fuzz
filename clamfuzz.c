d6f6fa1ce28606714e78299023d1ab52b746c8e5
Brandon Perry <bperry.volatile@gmail.com>
2016-05-04 20:24:17 +0000

    clam stuff

---
+++ clamfuzz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <clamav.h>
+
+int main(int argc, char *argv[])
+{
+    cl_init(CL_INIT_DEFAULT);
+    struct cl_engine *engine = cl_engine_new();
+    cl_engine_compile(engine);
+    const char *virusName = NULL;
+    long unsigned int scanned = 0;
+        cl_scanfile(argv[1], &virusName, &scanned, engine, CL_SCAN_STDOPT);
+    cl_engine_free(engine);
+}
